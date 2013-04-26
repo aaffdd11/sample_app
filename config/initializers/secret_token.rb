c882ca30131b5d382b4b807d9136a4a898852d9a
Eric <ychen@gigabase.org>
2013-04-26 17:41:14 +0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '54097e343336d47fab7a4422408ccef8547c75980f144f124e2664c0c149f309bf0dd617d11983e42760dbce87d1d4b5df6b8d2d780667e5e43eae2513012040'
